66e605ed0e816ca89545ed3680341545bed052a6
vnsemkin <vnsemkin@gmail.com>
2025-04-18 19:37:47 +0300

    Add send-message to chat logic

--- src/main/java/org/duckdns/celebritystrike/celebritystrike/exception/GlobalExceptionHandler.java
+++ src/main/java/org/duckdns/celebritystrike/celebritystrike/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.duckdns.celebritystrike.celebritystrike.model.Result;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageConversionException;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
